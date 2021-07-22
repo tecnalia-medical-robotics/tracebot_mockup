a2baafc7fc4624f9095e5d4cb7f5cecb7905a0e4
Ben Gordon <ben.gordon@astechprojects.co.uk>
2021-07-22 12:19:05 +0100

    tidied up gazebo launch code

--- tracebot_mockup_gazebo/scripts/gazebo_model_test.py
+++ tracebot_mockup_gazebo/scripts/gazebo_model_test.py
@@ -11,22 +11,22 @@ from std_msgs.msg import Duration
 from trajectory_msgs.msg import JointTrajectory
 from trajectory_msgs.msg import JointTrajectoryPoint
 
-def command_pub():
+def gazebo_model_test():
 
 	pub = rospy.Publisher('/pos_joint_traj_controller/command', JointTrajectory, queue_size=10)
-	rospy.init_node('command_pub', anonymous=True)
+	rospy.init_node('gazebo_model_test', anonymous=True)
 	rate = rospy.Rate(10) #10Hz
 	while not rospy.is_shutdown():
 		tracebot = JointTrajectory()
 		point = JointTrajectoryPoint()
 	
-		tracebot.joint_names = ['left_shoulder_pan_joint', 'left_shoulder_lift_joint', 'left_elbow_joint', 'left_wrist_1_joint', 'left_wrist_2_joint', 'left_wrist_3_joint']
+		tracebot.joint_names = ['left_shoulder_pan_joint', 'left_shoulder_lift_joint', 'left_elbow_joint', 'left_wrist_1_joint', 'left_wrist_2_joint', 'left_wrist_3_joint','right_shoulder_pan_joint', 'right_shoulder_lift_joint', 'right_elbow_joint', 'right_wrist_1_joint', 'right_wrist_2_joint', 'right_wrist_3_joint']
 		tracebot.header.stamp = rospy.Time.now()
 		
-		point.positions = [1.5,1.5,1.5,1.5,1.5,1.5]
-		point.velocities = [1.0,1.0,1.0,1.0,1.0,1.0]
-		point.accelerations = [1.0,1.0,1.0,1.0,1.0,1.0]
-		point.effort = [1.0,1.0,1.0,1.0,1.0,1.0]
+		point.positions = [1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5]
+		point.velocities = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
+		point.accelerations = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
+		point.effort = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
 		point.time_from_start = rospy.Duration(2)
 		tracebot.points.append(point)
 		
@@ -36,6 +36,6 @@ def command_pub():
 
 if __name__ == '__main__':
 	try:
-		command_pub()
+		gazebo_model_test()
 	except rospy.ROSInterruptException:
 		pass
