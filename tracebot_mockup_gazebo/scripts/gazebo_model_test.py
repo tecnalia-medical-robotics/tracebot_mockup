6e5b97808e60e72b2adda7635f998de001816b1c
Miguel Prada <miguel.prada@tecnalia.com>
2021-08-05 13:04:44 +0200

    Trim trailing whitespace

--- tracebot_mockup_gazebo/scripts/gazebo_model_test.py
+++ tracebot_mockup_gazebo/scripts/gazebo_model_test.py
@@ -14,17 +14,17 @@ def gazebo_model_test():
 	while not rospy.is_shutdown():
 		tracebot = JointTrajectory()
 		point = JointTrajectoryPoint()
-	
+
 		tracebot.joint_names = ['tracebot_left_arm_shoulder_pan_joint', 'tracebot_left_arm_shoulder_lift_joint', 'tracebot_left_arm_elbow_joint', 'tracebot_left_arm_wrist_1_joint', 'tracebot_left_arm_wrist_2_joint', 'tracebot_left_arm_wrist_3_joint','tracebot_right_arm_shoulder_pan_joint', 'tracebot_right_arm_shoulder_lift_joint', 'tracebot_right_arm_elbow_joint', 'tracebot_right_arm_wrist_1_joint', 'tracebot_right_arm_wrist_2_joint', 'tracebot_right_arm_wrist_3_joint']
 		tracebot.header.stamp = rospy.Time.now()
-		
+
 		point.positions = [1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5]
 		point.velocities = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
 		point.accelerations = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
 		point.effort = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
 		point.time_from_start = rospy.Duration(2)
 		tracebot.points.append(point)
-		
+
 		rospy.loginfo(tracebot)
 		pub.publish(tracebot)
 		rate.sleep()
